7c3edf355442fba4b4c0ae98071fb489196ed7aa
pradydevopsproj <prady26devops@gmail.com>
2026-08-13 18:58:14 +0000

    feat:tera fmt and regoin updated

--- lessons/day27/code/backend.tf
+++ lessons/day27/code/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket       = "tfstate-dev-prady-demo-ap-south-1-2wlagf-main-bkt" # Replace with your S3 bucket name
     key          = "terraform/state/main/terraform.tfstate"
-    region       = "us-east-1" # Replace with your region
-    use_lockfile = true        # S3 Native Locking (No DynamoDB needed)
+    region       = "ap-south-1" # Replace with your region
+    use_lockfile = true         # S3 Native Locking (No DynamoDB needed)
     encrypt      = true
   }
 }
