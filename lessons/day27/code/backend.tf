66df49c366f98434275cdfe18749bd00aee7b814
pradydevopsproj <prady26devops@gmail.com>
2026-08-16 07:31:58 +0000

    use myIP for SSH inbound allow

--- lessons/day27/code/backend.tf
+++ lessons/day27/code/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-dev-prady-demo-ap-south-1-2wlagf-main-bkt" # Replace with your S3 bucket name
+    bucket       = "tfstate-dev-roottt-ap-south-1-su0t0k-main-bkt" # Replace with your S3 bucket name
     key          = "terraform/state/main/terraform.tfstate"
     region       = "ap-south-1" # Replace with your region
     use_lockfile = true         # S3 Native Locking (No DynamoDB needed)
